8d8ffd08c7af34e18d1f422374b8c01ee914b12a
Abdullahi <Abdullahinuur2007@gmail.com>
2025-12-16 09:15:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Cart Open/Close
+let cart = document.querySelector(".cart");
+
+//Open Cart
+function openCart() {
+  cart.style.display = "block"
+}
+//close Cart
+function closeCart() {
+  cart.style.display = "none"
+}
+
